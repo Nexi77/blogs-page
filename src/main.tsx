1437e9776e9555440d4264cf05f53854c48b72d9
Konrad Kolasa <kolasakonrad1@gmail.com>
2022-11-22 12:28:43 +0100

    install react-query and move router config

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from "react-router-dom"
+import { router } from "./config/router"
 import "./assets/styles/styles.scss"
-import Home from './pages/home/views/Home'
 
-const router  = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: '/',
-        element: <Home />
-      }
-    ]
-  }
-])
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
