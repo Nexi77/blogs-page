6a1bfd027945d97789c11064b3ac7e81f1320e5c
Konrad Kolasa <kolasakonrad1@gmail.com>
2022-11-22 20:57:55 +0100

    test smoother animation

--- src/pages/services/views/Services.tsx
+++ src/pages/services/views/Services.tsx
@@ -11,7 +11,7 @@ function Services(){
                 <motion.div
                     initial={{ opacity: 0, scale: 0.5 }}
                     whileInView={{ opacity: 1, scale: 1 }}
-                    viewport={{ once: true, amount: 0.5 }}
+                    viewport={{ once: true }}
                     transition={{ duration: 0.5, staggerChildren: 0.4 }}
                     className={service.text_section}
                 >
